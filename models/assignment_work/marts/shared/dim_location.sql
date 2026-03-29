05c55f94e49cad4603974e482342a4d92d1f6260
HUI GAO <hui.gao1@baruchmail.cuny.edu>
2026-03-29 06:22:31 +0000

    fixed alias zip_code

--- models/assignment_work/marts/shared/dim_location.sql
+++ models/assignment_work/marts/shared/dim_location.sql
@@ -3,7 +3,7 @@
 WITH all_locations AS (
    -- Get locations from 311 requests
    SELECT DISTINCT
-      CAST(incident_zip AS STRING) AS incident_zip --TODO replace (HINT: look @ dimensional model & staging data!)
+      CAST(incident_zip AS STRING) AS zip_code --TODO replace (HINT: look @ dimensional model & staging data!)
    FROM {{ ref('stg_nyc_311_dot') }}
    WHERE borough IS NOT NULL
 
