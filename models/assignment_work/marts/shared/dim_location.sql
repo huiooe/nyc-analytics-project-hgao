e44158f7a6484d33697cb28bf5340759356ee953
HUI GAO <hui.gao1@baruchmail.cuny.edu>
2026-03-29 06:25:04 +0000

    fixed both borough and zip

--- models/assignment_work/marts/shared/dim_location.sql
+++ models/assignment_work/marts/shared/dim_location.sql
@@ -3,6 +3,7 @@
 WITH all_locations AS (
    -- Get locations from 311 requests
    SELECT DISTINCT
+      borough,
       CAST(incident_zip AS STRING) AS zip_code --TODO replace (HINT: look @ dimensional model & staging data!)
    FROM {{ ref('stg_nyc_311_dot') }}
    WHERE borough IS NOT NULL
@@ -11,7 +12,8 @@ WITH all_locations AS (
 
    -- Get locations from restaurant applications
    SELECT DISTINCT
-       borough -- TODO replace (HINT: look @ dimensional model & staging data!)
+       borough
+       CAST(zip AS STRING) AS zip_code-- TODO replace (HINT: look @ dimensional model & staging data!)
    FROM {{ ref('stg_nyc_open_restaurant_apps') }}
    WHERE borough IS NOT NULL
 ),
