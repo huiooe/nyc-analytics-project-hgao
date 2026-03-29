9023cdd6bfdf639c7f467181969411f23ca6c500
HUI GAO <hui.gao1@baruchmail.cuny.edu>
2026-03-29 06:26:02 +0000

    added comma and run

--- models/assignment_work/marts/shared/dim_location.sql
+++ models/assignment_work/marts/shared/dim_location.sql
@@ -12,7 +12,7 @@ WITH all_locations AS (
 
    -- Get locations from restaurant applications
    SELECT DISTINCT
-       borough
+       borough,
        CAST(zip AS STRING) AS zip_code-- TODO replace (HINT: look @ dimensional model & staging data!)
    FROM {{ ref('stg_nyc_open_restaurant_apps') }}
    WHERE borough IS NOT NULL
